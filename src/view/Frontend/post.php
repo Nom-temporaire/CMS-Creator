d0202d30fef1837ab4454eacf502cead68c57279
DestCom <azevedo-a@hotmail.fr>
2021-12-07 23:36:24 +0100

    add css

--- src/view/Frontend/post.php
+++ src/view/Frontend/post.php
@@ -1,24 +1,28 @@
-<div class="mt-40 flex flex-col">
+<div class="mt-40 flex flex-col text-xl">
     <div>
-        <h1>Mon super titre</h1>
+        <h1 class="font-bold my-4 text-2xl">Mon super titre</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quis quae atque sequi eum placeat distinctio.
             Ipsam, nihil voluptates aperiam sunt doloribus hic neque repellat vitae enim dolores labore accusamus.<br />
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus nisi, non dolorum odit sit dicta unde
             eius. Tempora molestias dolore tempore vitae. Voluptatem fugiat ducimus itaque beatae aut tenetur sed!</p>
         <h4>Fait par user - le 11/11/1111</h4>
     </div>
-    <div class="commentaires">
-        <div>
-            <h4>UserCommentary</h4>
-            <p>Le 11/11/1111</p>
+    <div class="mt-40">
+        <div class="my-8">
+            <div class="flex">
+                <h4 class="mr-4">UserCommentary</h4>
+                <p class="ml-4">Le 11/11/1111</p>
+            </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, fuga? Architecto eveniet deleniti,
                 ipsam
                 tenetur optio voluptas doloremque autem velit neque ipsa dignissimos dolor est. Quaerat rerum omnis
                 voluptate vitae.</p>
         </div>
-        <div>
-            <h4>UserCommentary</h4>
-            <p>Le 11/11/1111</p>
+        <div class="my-8">
+            <div class="flex">
+                <h4 class="mr-4">UserCommentary</h4>
+                <p class="ml-4">Le 11/11/1111</p>
+            </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, fuga? Architecto eveniet deleniti,
                 ipsam
                 tenetur optio voluptas doloremque autem velit neque ipsa dignissimos dolor est. Quaerat rerum omnis
