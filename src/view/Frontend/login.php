d5990626361076bde3631a15cc103af9abfc64f3
DestCom <azevedo-a@hotmail.fr>
2021-12-08 14:27:13 +0100

    change css

--- src/view/Frontend/login.php
+++ src/view/Frontend/login.php
@@ -1,4 +1,4 @@
-<div class="w-4/12 mx-auto mt-80 h-2/6 p-8 bg-gray-300 rounded-lg flex flex-col items-center justify-center">
+<div class="w-4/12 mx-auto mt-20 h-2/6 p-8 bg-gray-300 rounded-lg flex flex-col items-center justify-center">
     <h1>Login</h1>
     <form action="login.php" method="post" class="m-auto flex flex-col">
         <label for="username" class="m-2">Username</label>
--- src/view/Frontend/signup.php
+++ src/view/Frontend/signup.php
@@ -1,4 +1,4 @@
-<div class="w-4/12 mx-auto mt-80 h-2/6 p-8 bg-gray-300 rounded-lg flex flex-col items-center justify-center">
+<div class="w-4/12 mx-auto mt-20 h-2/6 p-8 bg-gray-300 rounded-lg flex flex-col items-center justify-center">
     <h1>Sign-up</h1>
     <form action="create" method="post" class="m-auto flex flex-col">
         <label for="username" class="m-2">Username</label>
@@ -6,7 +6,8 @@
         <label for="password" class="m-2">Password</label>
         <input type="password" name="password" id="password" class="m-2" required>
         <label for="email" class="m-2">Email</label>
-        <input type="text" name="email" id="email" class="m-2" pattern="^[a-zA-Z0-9]+[@]+[a-zA-Z0-9]+[.]+[a-z]{2,3}" required>
+        <input type="text" name="email" id="email" class="m-2" pattern="^[a-zA-Z0-9]+[@]+[a-zA-Z0-9]+[.]+[a-z]{2,3}"
+            required>
         <input type="submit" value="Sign-up" class="m-2">
     </form>
 </div>
