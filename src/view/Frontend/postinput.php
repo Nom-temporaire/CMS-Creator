1d423c6c15c59cacc87a5b9f17a511cb21ba67e2
Hiteazel <khacminhbrandon.vo@gmail.com>
2021-12-10 15:59:45 +0100

    Modification multiples sur les pages

--- src/view/Frontend/postinput.php
+++ src/view/Frontend/postinput.php
@@ -5,20 +5,20 @@ if ($_SESSION['role'] == 'visiteur') {
 ?>
 
 <div class="w-full mt-20">
-    <form action="createpost" method="post" enctype="multipart/form-data">
+    <form action="createpost" method="post" class="w-10/12 mx-auto mt-20 h-6/6 p-8 bg-gray-900 rounded-lg flex flex-col items-left justify-left">
         <div class="mb-6">
-            <label for="titre" class="text-sm font-medium text-gray-900 block mb-2">Title</label>
-            <input type="text" id="titre" name="titre" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="My amazing title" required="">
+            <label for="titre" class="text-2xl font-medium text-gray-200 block mb-2 ml-5">Titre</label>
+            <input type="text" id="titre" name="titre" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Votre titre" required="">
         </div>
 
         <div class="mb-6">
-            <label for="image" class="text-sm font-medium text-gray-900 block mb-2">Image</label>
-            <input type="file" id="file" name="file" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="My amazing title" required="">
+            <label for="image" class="text-2xl font-medium text-gray-200 block mb-2 ml-5">Image</label>
+            <input type="file" id="file" name="file" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="IMAGE" required="">
         </div>
         <div class="mb-6">
-            <label for="contenu" class="text-sm font-medium text-gray-900 block mb-2">Content</label>
-            <textarea id="contenu" name="contenu" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="My amazing post !" required=""></textarea>
+            <label for="contenu" class="text-2xl font-medium text-gray-200 block mb-2 ml-5">Contenu</label>
+            <textarea id="contenu" name="contenu" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Contenu de votre message" required=""></textarea>
         </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Poster</button>
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center float-right">Poster</button>
     </form>
 </div>
