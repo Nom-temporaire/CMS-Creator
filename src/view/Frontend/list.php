ec3f6bb57ea921dc2b5525f98eb9950c017e9b4c
ADR1811 <adrialb95@gmail.com>
2021-12-10 14:11:04 +0100

    ok

--- src/view/Frontend/list.php
+++ src/view/Frontend/list.php
@@ -10,13 +10,13 @@ $result = $post->getAllPosts();
 
 <div class="w-full mt-20 flex flex-col text-gray-200">
     <?php foreach ($result as $post) : ?>
-    <div class="m-6 p-2 border-2 border-black">
-        <h3 class="font-bold"><?= $post->getTitle() ?></h3>
-        <p class="my-2"><?= $post->getContent() ?></p>
-        <div class="w-full flex justify-between">
-            <p><?= $post->getDate() ?></p>
-            <a class="text-right" <?= 'href="/post/' . $post->getIdPost() .'"' ?>>Voir plus ...</a>
+        <div class="m-6 p-2 border-2 border-black">
+            <h3 class="font-bold"><?= $post->getTitle() ?></h3>
+            <p class="my-2"><?= $post->getContent() ?></p>
+            <div class="w-full flex justify-between">
+                <p><?= $post->getDate() ?></p>
+                <a class="text-right" <?= 'href="/post/' . $post->getIdPost() . '"' ?>>Voir plus ...</a>
+            </div>
         </div>
-    </div>
     <?php endforeach; ?>
 </div>
