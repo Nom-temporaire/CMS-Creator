85b5257eac037741e7b9d3aacb0db534967ee174
DestCom <azevedo-a@hotmail.fr>
2021-12-10 15:49:34 +0100

    Deletion ended

--- src/view/Frontend/deletepost.php
+++ src/view/Frontend/deletepost.php
@@ -1,12 +1,28 @@
 <?php
 
 use App\Manager\PostManager;
+use App\Manager\CommentManager;
+use App\Manager\ImageManager;
 use App\Fram\Factories\PDOFactory;
 
 if($_SESSION['role'] == 'visiteur')
 {
     header('Location: /');
 }
+
+$deleteComments = new CommentManager(PDOFactory::getMysqlConnection());
+$deleteComments->deleteCommentsFromPost($id);
+
+$deleteFile = new ImageManager(PDOFactory::getMysqlConnection());
+$result = $deleteFile->getCheminsFromPost($id);
+
+foreach ($result as $image) {
+    $src = 'public/images/'.$image->getChemin();
+    unlink($src);
+}
+    
+$deleteImages = new ImageManager(PDOFactory::getMysqlConnection());
+$deleteImages->deleteImagesFromPost($id);
 
 $post = new PostManager(PDOFactory::getMysqlConnection());
 $post->deletePost($id);
