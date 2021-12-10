539a082f006d40dac934328be2ff31e311b5508c
ADR1811 <adrialb95@gmail.com>
2021-12-10 09:18:47 +0100

    c'est plus beau que celui d'antoine voilà c'est dit mtn tu vas faire quoi. Spoiler : Absolument rien

--- src/Manager/PostManager.php
+++ src/Manager/PostManager.php
@@ -4,8 +4,10 @@ namespace App\Manager;
 
 use App\Entity\Post;
 
-class PostManager extends BaseManager{
-    public function createPost($title, $content, $authorID){
+class PostManager extends BaseManager
+{
+    public function createPost($title, $content, $authorID)
+    {
         $this->post = new Post();
         $this->post->setTitle($title);
         $this->post->setContent($content);
@@ -24,7 +26,8 @@ class PostManager extends BaseManager{
         $request->execute($data);
     }
 
-    public function getPost($id){
+    public function getPost($id)
+    {
         $select = "SELECT * FROM post WHERE idPost = :idPost";
         $this->pdoStatement = $this->pdo->prepare($select);
         $this->pdoStatement->bindValue('idPost', $id, \PDO::PARAM_INT);
@@ -34,7 +37,8 @@ class PostManager extends BaseManager{
         return $result;
     }
 
-    public function deletePost($post){
+    public function deletePost($post)
+    {
         $delete = "DELETE FROM post WHERE id = :id LIMIT 1";
         $this->pdoStatement = $this->pdo->prepare($delete);
         $this->pdoStatement->bindValue('id', $post->getId(), \PDO::PARAM_INT);
@@ -42,15 +46,16 @@ class PostManager extends BaseManager{
         return $this->pdoStatement->execute();
     }
 
-    public function getAllPosts(){
+    public function getAllPosts()
+    {
 
         $results = [];
 
         $select = "SELECT * FROM post";
 
-       $this->pdoStatement = $this->pdo->query($select);
-        
-        while($post = $this->pdoStatement->fetchObject('App\Entity\Post')){
+        $this->pdoStatement = $this->pdo->query($select);
+
+        while ($post = $this->pdoStatement->fetchObject('App\Entity\Post')) {
             $results[] = $post;
         }
 
